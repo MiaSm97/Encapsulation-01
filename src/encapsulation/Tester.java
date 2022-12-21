94236c90e5b18ad2685a9c633b9f2b5545dd245f
Mia Smaldone <eufosm@libero.it>
2022-12-21 11:28:48 +0100

    Ho provato a rendere l'esercizio più leggibile

--- src/encapsulation/Tester.java
+++ src/encapsulation/Tester.java
@@ -7,10 +7,19 @@ public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         House house = new House();
+        System.out.println("Insert your address:");
         house.setAddress(scanner.nextLine());
+
+        System.out.println("How many floors has your house?");
         house.setFloorsNumber(scanner.nextInt());
+
+        System.out.println("Who are the residents in your house?");
         house.setResidentsNames(scanner.next().split(","));
-        System.out.printf(house.getAddress() + house.getFloorsNumber() + Arrays.toString(house.getResidentsNames()));
+
+        System.out.printf(house.getAddress() + "\n");
+        System.out.printf(house.getFloorsNumber() + "\n");
+        System.out.printf(Arrays.toString(house.getResidentsNames()));
+
 
     }
 }
